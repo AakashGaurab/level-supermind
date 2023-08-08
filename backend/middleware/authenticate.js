137896cc975dbbb7cb59314bd8b6793416563224
Aakash Gaurab <59801581+AakashGaurab@users.noreply.github.com>
2023-08-08 22:04:34 +0530

    debugged cookie error

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -2,8 +2,14 @@ const jwt = require("jsonwebtoken");
 
 
 const auth = (req,res,next)=>{
-    let token = req.cookies.normal_token;          //cookie system doesnot work as it gives undefined and due to lack of time 
-    next();
+    jwt.verify(token,"blog",(err,decoded)=>{
+        if (decoded){
+            next();
+        }
+        else {
+            res.json([]);
+        }
+    })
 }
 
 module.exports={auth};
